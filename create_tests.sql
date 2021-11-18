334d572941f8921a069e430d5c553ad24bc367d3
深澤　寛太 <kantafkzw@gmail.com>
2021-11-18 22:17:41 +0900

    テーブルを作成

---
+++ create_tests.sql
@@ -0,0 +1,17 @@
+create table tests (
+    raceno     int         primary key,
+    events     varchar(16) not null,
+    groups     varchar(6),
+    rank1      int,
+    crew_rank1 text,
+    rank2      int,
+    crew_rank2 text,
+    rank3      int,
+    crew_rank3 text,
+    rank4      int,
+    crew_rank4 text,
+    rank5      int,
+    crew_rank5 text,
+    rank6      int,
+    crew_rank6 text
+);
